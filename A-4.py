c6f2d565b0a097fbd6ec76ac372e107c91367a8b
BabluSingh7 <bablusinghkb1@gmail.com>
2026-07-04 12:24:27 +0530

    Assign No-4

---
+++ A-4.py
@@ -0,0 +1,29 @@
+#q1
+x=11
+print(x)
+
+#q2
+n= input("Enter your name:")
+print("Hello,", n)
+
+#q3
+a,b,c = int(input("Enter the three numbers:")),int(input()),int(input())
+print("A=",a,"\nB=",b,"\nC=",c)
+
+#q4
+a = 35
+b =True
+s = "MysirG"
+f = 5.46
+c = 2+9j
+print(type(a),a)
+print(type(b),b)
+print(type(s),s)
+print(type(f),f)
+print(type(c),c)
+
+#q5
+d=4
+m=7
+y=2026
+print("Date:",d,"/",m,"/",y)
